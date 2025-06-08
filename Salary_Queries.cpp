b8f50ab0945239d74b450dae8d3b054f26c9ce1c
Muttakin Ahmed <muttakinbgd@gmail.com>
2025-06-08 20:15:58 +0600

    New solution

--- Salary_Queries.cpp
+++ Salary_Queries.cpp
@@ -1,63 +1,115 @@
-// [ ﷽ ] Bismillahir Rahmanir Raheem//
+// [ ﷽ ] Bismillahir Rahmanir Raheem
 #include <bits/stdc++.h>
+
 using namespace std;
-#define test int t; cin >> t; while(t--)
-#define N(n) int n; cin >> n;
+
+// ========== Shortcuts ==========
 #define ll long long
 #define f(i,a,b) for(int i=int(a);i<int(b);i++)
-#define fr(i,a,b) for(int i =int(a);i>int(b); i--)
-#define A(a,n) vector<int> a(n); f(i,0,n) cin >> a[i];
+#define fr(i,a,b) for(int i=int(a);i>int(b);i--)
+#define test int TTT; cin >> TTT; while(TTT--)
+#define cases int TTT; cin >> TTT; f(i,1,TTT+1) { cout << "Case " << i << ": ";
+#define endcases }
+#define A(a,n) vector<ll> a(n); f(i,0,n) cin >> a[i];
 #define pb push_back
-#define bg begin()
-#define ed end()
-#define sz size()
-#define pii pair<int,int>
-#define vi vector<int>
-#define mii map<int,int>
-#define vpi vector<pair<int,int>>
+#define pii pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define mii map<ll,ll>
 #define SORT(x) sort(x.begin(),x.end())
-#define ALL(x) (x.begin(), x.end())
+#define ALL(x) (x).begin(), (x).end()
 #define YY cout << "YES\n"
 #define NN cout << "NO\n"
-#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define setbit(x,k) ((x) | (1LL<<(k)))
+#define clrbit(x,k) ((x) & ~(1LL<<(k)))
+#define checkbit(x,k) (((x)>>(k))&1)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); cout << fixed << setprecision(10);
 #define warida return
 
-const int mod=1e9+7;
-const int INF = 2147483647;
+// ========== Debugging ==========
+#ifndef ONLINE_JUDGE
 
+// 1) Arithmetic types
+template<typename T, typename std::enable_if<std::is_arithmetic<T>::value, int>::type = 0>
+void __print(T x) { cerr << x; }
 
-ll inv(ll a, ll m) {
-return a <= 1 ? a : m - (long long)(m/a) * inv(m % a, m) % m;}
+// 2) Char, C-string, string, bool
+void __print(char x)              { cerr << '\'' << x << '\''; }
+void __print(const char* x)       { cerr << '"' << x << '"'; }
+void __print(const std::string &x){ cerr << '"' << x << '"'; }
+void __print(bool x)              { cerr << (x ? "true" : "false"); }
 
+// 3) pair<>
+template<typename T, typename V>
+void __print(const std::pair<T, V> &x) { cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}'; }
 
-ll bigmod(ll a, ll b, ll m){
-    //a^b (mod m)
-    if(b==0) return 1;
-    if(b==1) return a%m;
-    ll x = bigmod(a,b/2,m);
-    if(b%2) return (((x*x)%m)*a)%m;
-    return (x*x)%m;
-}
+// 4) Generic containers (non-arithmetic)
+template<typename T, typename std::enable_if<!std::is_arithmetic<T>::value, int>::type = 0>
+void __print(const T &x) { int f=0; cerr<<'{'; for(auto &i : x) cerr<<(f++?", ":"") , __print(i); cerr<<'}'; }
+
+// 5) _debug dispatcher
+void _debug() { cerr << "]\n"; }
+template<typename T, typename... V>
+void _debug(T t, V... v) { __print(t); if(sizeof...(v)) cerr << ", "; _debug(v...); }
+
+// 6) debug macro
+#define debug(...) do { cerr << "LINE(" << __LINE__ << "): [" #__VA_ARGS__ "] = ["; _debug(__VA_ARGS__); } while(0)
+
+#else
+#define debug(...) do{}while(0)
+#endif
+
+// ========== Optional Ordered Set Stuff ==========
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+// // run in terminal, cph e kaj kore na
+
+// ========== Constants ==========
+const int mod = 1e9+7;
+const ll INF = LLONG_MAX;
+const int inf = INT_MAX;
+const double eps = 1e-9;
+
+// ========== Functions ==========
+ll inv(ll a, ll m = mod) { return a <= 1 ? a : m - (ll)(m/a) * inv(m % a, m) % m; }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a,b) * b; }
+ll bigmod(ll a, ll b, ll m = mod) { if(b==0) return 1; ll x = bigmod(a,b/2,m); return (b&1)?(x*x%m)*a%m:(x*x%m); }
+
 
 //https://cses.fi/problemset/task/1144
 
-int main(){
-
+int main() {
+    fast
+    ordered_set<pair<int,int> > s;
     int n, q;
     cin >> n >> q;
-    map<int,int> mp;
-    vector<int> a(n+1);
+    int v[n+1];
     f(i,0,n){
         int x;
         cin >> x;
-        a[i+1]= x;
-        mp[x]++;
+        v[i+1]=x;
+        s.insert(make_pair(x,i+1));
     }
     while(q--){
-        char x;
-        cin >> x;
-        
-        if(x=='?') cout << mp[a[x]] << endl;
+        char a;
+        int x, y;
+        cin >>a >> x >> y;
+        if(a=='!'){
+            s.erase(make_pair(v[x],x));
+            v[x]=y;
+            s.insert(make_pair(y,x));
+        }
+        else{
+            cout << s.order_of_key(make_pair(y,inf)) - s.order_of_key(make_pair(x, 0)) << endl;
+        }
     }
-
 }
+
+
